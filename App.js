b37f4846a00ebabd2ad0e17d3dba78c66cefff0b
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-05-25 16:15:20 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity , View } from 'react-native';
 
 export default class App extends React.Component {
+
+state = {
+  counter :0
+}
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={{flexDirection:'row',width:200, justifyContent:'space-around'}}>
+        <TouchableOpacity onPress={()=>this.increaseCounter()}>
+          <Text style={{fontSize:20}}>Increase</Text>
+        </TouchableOpacity>
+        
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => this.decreaseCounter()}>
+            <Text style={{ fontSize: 20 }}>Decrease</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
