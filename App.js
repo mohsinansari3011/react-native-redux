e949d8b3b818722846157e7c24e8e5a3f9128fb8
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-05-25 16:33:57 +0500

    app

--- App.js
+++ App.js
@@ -1,38 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity , View } from 'react-native';
+import { createStore } from 'redux'
+import CounterApp from './src/CounterApp.js'
+
+
+
+
+const reducer = () => {
+}
+
+const store = createStore(reducer)
+
 
 export default class App extends React.Component {
 
-state = {
-  counter :0
-}
+
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{flexDirection:'row',width:200, justifyContent:'space-around'}}>
-        <TouchableOpacity onPress={()=>this.increaseCounter()}>
-          <Text style={{fontSize:20}}>Increase</Text>
-        </TouchableOpacity>
-        
-          <TouchableOpacity>
-            <Text style={{ fontSize: 20 }}>{this.state.counter}</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => this.decreaseCounter()}>
-            <Text style={{ fontSize: 20 }}>Decrease</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      <CounterApp/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
